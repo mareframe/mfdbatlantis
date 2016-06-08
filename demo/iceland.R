497ae5253d5b1070ae5798ef59f7f4f85264a9d1
Jamie Lentin <jm@lentin.co.uk>
2016-06-08 12:21:07 +0100

    Change iceland prefix, so comparing the 2 demos is easier

--- demo/iceland.R
+++ demo/iceland.R
@@ -5,45 +5,45 @@ library(utils)
 mdb <- mfdb('Atlantis-Iceland')
 
 # Find all the files in this directory
-ice_dir <- 'atlantis-Iceland-2016-05-24/'  # TODO: Shouldn't be in the demo either
-attr(ice_dir, 'start_year') <- 1948
+is_dir <- 'atlantis-Iceland-2016-05-24/'  # TODO: Shouldn't be in the demo either
+attr(is_dir, 'start_year') <- 1948
 
-ice_run_options <- atlantis_run_options(ice_dir)
+is_run_options <- atlantis_run_options(is_dir)
 
 # Read in areas / surface temperatures, insert into mfdb
-ice_area_data <- atlantis_read_areas(ice_dir)
-ice_temp <- atlantis_tracer(ice_dir, ice_area_data, 'Temp')
-mfdb_import_area(mdb, ice_area_data)
-ice_temp$temperature <- ice_temp$value
-mfdb_import_temperature(mdb, ice_temp[ice_temp$depth == 1,])
+is_area_data <- atlantis_read_areas(is_dir)
+is_temp <- atlantis_tracer(is_dir, is_area_data, 'Temp')
+mfdb_import_area(mdb, is_area_data)
+is_temp$temperature <- is_temp$value
+mfdb_import_temperature(mdb, is_temp[is_temp$depth == 1,])
 
 # Read in all functional groups, assign MFDB shortcodes where possible
-ice_functional_groups <- atlantis_functional_groups(ice_dir)
-ice_functional_groups$MfdbCode <- vapply(
-    mfdb_find_species(ice_functional_groups$LongName)['name',],
+is_functional_groups <- atlantis_functional_groups(is_dir)
+is_functional_groups$MfdbCode <- vapply(
+    mfdb_find_species(is_functional_groups$LongName)['name',],
     function (x) if (length(x) > 0) x[[1]] else as.character(NA), "")
 
 # Ingest survey data
 for (fgName in c("Cod", "Haddock")) {
-    fg <- ice_functional_groups[c(ice_functional_groups$Name == fgName),]
+    fg <- is_functional_groups[c(is_functional_groups$Name == fgName),]
     cat("Importing functional group", fg$Name, "\n")
 
-    ice_fg_count <- atlantis_fg_tracer(ice_dir, ice_area_data, fg)
-    ice_fg_count$species <- fg$MfdbCode
-    ice_fg_count$areacell <- ice_fg_count$area
+    is_fg_count <- atlantis_fg_tracer(is_dir, is_area_data, fg)
+    is_fg_count$species <- fg$MfdbCode
+    is_fg_count$areacell <- is_fg_count$area
     # TODO: More selectivity (Look with Bjarki at Rgadget mean length simulat)
     # TODO: Want a length selectivity curve for the gear, areas that you're interested in.
     # TODO: Apply this also to consumption below, maybe combine the two?
-    mfdb_import_survey(mdb, ice_fg_count, data_source = paste0('atlantisTracer_', fgName))
+    mfdb_import_survey(mdb, is_fg_count, data_source = paste0('atlantisTracer_', fgName))
 }
 
 for (fgName in c("Cod")) {
-    fg <- ice_functional_groups[c(ice_functional_groups$Name == fgName),]
+    fg <- is_functional_groups[c(is_functional_groups$Name == fgName),]
 
     # Fetch consumption and tracer indexes for functional group
     consumption <- atlantis_fg_tracer(
-        ice_dir,
-        ice_area_data,
+        is_dir,
+        is_area_data,
         fg_group = fg,
         consumption = TRUE)
 
@@ -57,7 +57,7 @@ for (fgName in c("Cod")) {
     #   => Don't bother with this, only generating proportions anyway
 
     # Convert this into the 2 data.frames import_stomach requires
-    stomach <- atlantis_stomach_content(ice_dir, consumption, predator_map = c(
+    stomach <- atlantis_stomach_content(is_dir, consumption, predator_map = c(
         FCD = 'COD'
     ), prey_map = c(
         # We're only interested in 2 species
@@ -67,33 +67,33 @@ for (fgName in c("Cod")) {
     mfdb_import_stomach(mdb, stomach$predator_data, stomach$prey_data, data_source = "stomach_Cod")
 }
 
-ice_fisheries <- atlantis_fisheries(ice_dir)
+is_fisheries <- atlantis_fisheries(is_dir)
 mfdb_import_vessel_taxonomy(mdb, data.frame(
-    id = ice_fisheries$Index,
-    name = ice_fisheries$Code,
-    full_name = ice_fisheries$Name,
+    id = is_fisheries$Index,
+    name = is_fisheries$Code,
+    full_name = is_fisheries$Name,
     stringsAsFactors = FALSE))
 
-for (fisheryCode in ice_fisheries$Code) {
-    fishery <- ice_fisheries[ice_fisheries$Code == fisheryCode,]
+for (fisheryCode in is_fisheries$Code) {
+    fishery <- is_fisheries[is_fisheries$Code == fisheryCode,]
     cat("Importing fishery", fisheryCode, "\n")
 
-    ice_catch <- atlantis_fisheries_catch(ice_dir, ice_area_data, fishery)
-    if (nrow(ice_catch) == 0) next
+    is_catch <- atlantis_fisheries_catch(is_dir, is_area_data, fishery)
+    if (nrow(is_catch) == 0) next
 
     # Species column that maps to MFDB code
-    ice_catch$species <- ice_catch$functional_group
-    levels(ice_catch$species) <- ice_functional_groups[match(
-        levels(ice_catch$functional_group),
-        ice_functional_groups$GroupCode), 'MfdbCode']
+    is_catch$species <- is_catch$functional_group
+    levels(is_catch$species) <- is_functional_groups[match(
+        levels(is_catch$functional_group),
+        is_functional_groups$GroupCode), 'MfdbCode']
 
     mfdb_import_survey(mdb, data.frame(
-        year = ice_catch$year,
-        month = ice_catch$month,
-        areacell = ice_catch$area,
-        vessel = ice_catch$fishery,
-        species = ice_catch$species,
-        weight_total = ice_catch$weight_total,
+        year = is_catch$year,
+        month = is_catch$month,
+        areacell = is_catch$area,
+        vessel = is_catch$fishery,
+        species = is_catch$species,
+        weight_total = is_catch$weight_total,
         count = c(NA),
         stringsAsFactors = TRUE), data_source = paste0("atlantisFishery_", fisheryCode))
 }
